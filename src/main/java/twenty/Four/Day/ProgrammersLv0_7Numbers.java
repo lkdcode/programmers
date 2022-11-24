ba959e3aa4de33c440f380ab76c6ee452d53b504
lkdcode <dlrlejr1@naver.com>
2022-11-24 23:57:17 +0900

    ✨Feat : 7의 개수
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Four/Day/ProgrammersLv0_7Numbers.java
@@ -0,0 +1,20 @@
+package twenty.Four.Day;
+
+public class ProgrammersLv0_7Numbers {
+    public static void main(String[] args) {
+        int[] array = {7, 77, 17};
+        /*
+         * 7의 개수
+         */
+        String str = "";
+        for (int i = 0; i < array.length; i++) {
+            str += (String.valueOf(array[i]));
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '7') {
+            }
+        }
+
+    }
+}
