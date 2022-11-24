1d92529cb9b668f2961262dae7ae4e543d0c9f02
lkdcode <dlrlejr1@naver.com>
2022-11-24 12:31:03 +0900

    ✨Feat : 약수 구하기
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Four/Day/ProgrammersLv0_measure.java
@@ -0,0 +1,33 @@
+package twenty.Four.Day;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProgrammersLv0_measure {
+    public static void main(String[] args) {
+        int n = 16;
+        /*
+         * 약수 구하기
+         */
+
+        int a = (int) Math.sqrt(n);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= a; i++) {
+            if (n % i == 0) {
+                list.add(i);
+                if (n / i != i) {
+                    list.add(n / i);
+                }
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list);
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+
+    }
+}
