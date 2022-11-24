403543b8aedb95edf504f08b404cc7d23248702b
lkdcode <dlrlejr1@naver.com>
2022-11-25 00:00:25 +0900

    ✨Feat : 합성수 찾기
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Four/Day/ProgrammersLv0_CompositeNumber.java
@@ -0,0 +1,24 @@
+package twenty.Four.Day;
+
+public class ProgrammersLv0_CompositeNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        /*
+         * 제목 기입
+         */
+        int answer = 0;
+
+        for (int i = 4; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                answer++;
+            }
+        }
+
+    }
+}
