b9c9273daa4752205fba13c9aa95e47661b5f8a1
lkdcode <dlrlejr1@naver.com>
2022-11-26 23:43:50 +0900

    ✨Feat : 구슬을 나누는 경우의 수
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Six/Day/ProgrammersLv0_BallShare.java
@@ -0,0 +1,35 @@
+package twenty.Six.Day;
+
+import java.math.BigInteger;
+
+public class ProgrammersLv0_BallShare {
+    public static void main(String[] args) {
+
+        int balls = 30;
+        int share = 2;
+
+        /*
+         * 구슬을 나누는 경우의 수
+         */
+
+        BigInteger n = new BigInteger("1");
+        BigInteger m = new BigInteger("1");
+
+        for (int i = 1; i <= balls; i++) {
+            n = n.multiply(BigInteger.valueOf(i));
+        }
+        for (int i = 1; i <= share; i++) {
+            m = m.multiply(BigInteger.valueOf(i));
+        }
+        BigInteger nm = new BigInteger("1");
+        for (int i = 1; i <= (balls - share); i++) {
+            nm = nm.multiply(BigInteger.valueOf(i));
+        }
+
+        BigInteger answer = new BigInteger("1");
+
+        answer = n.divide(nm.multiply(m));
+        System.out.println(answer);
+
+    }
+}
