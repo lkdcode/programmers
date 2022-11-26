6bd293f87861237234b84687a742b72594ad6eb2
lkdcode <dlrlejr1@naver.com>
2022-11-26 23:44:37 +0900

    ✨Feat : 문자열 계산하기
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Six/Day/ProgrammersLv0_Stringcal.java
@@ -0,0 +1,41 @@
+package twenty.Six.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammersLv0_Stringcal {
+    public static void main(String[] args) {
+        String my_string = "3 + 44 - 15 + 5";
+        /*
+         * 문자열 계산하기
+         */
+        int answer = 0;
+        String[] str = my_string.split(" ");
+        List<Integer> numbers = new ArrayList<>();
+        List<String> cal = new ArrayList<>();
+
+        for (int i = 0; i < str.length; i++) {
+            if (!(str[i].equals("+")) && !(str[i].equals("-"))) {
+                numbers.add(Integer.parseInt(str[i]));
+            }
+            if (str[i].equals("+") || str[i].equals("-")) {
+                cal.add(str[i]);
+            }
+        }
+
+        System.out.println(numbers);
+        System.out.println(cal);
+        answer = numbers.get(0);
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (cal.get(i).equals("+")) {
+                answer += numbers.get(i + 1);
+            }
+
+            if (cal.get(i).equals("-")) {
+                answer -= numbers.get(i + 1);
+            }
+        }
+
+    }
+}
