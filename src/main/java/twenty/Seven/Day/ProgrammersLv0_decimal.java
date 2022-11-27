93532485099ccf53fd5c6c69a1eb1c074f5941ee
lkdcode <dlrlejr1@naver.com>
2022-11-27 22:27:04 +0900

    ✨Feat : 유한소수 판별하기
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Seven/Day/ProgrammersLv0_decimal.java
@@ -0,0 +1,43 @@
+package twenty.Seven.Day;
+
+public class ProgrammersLv0_decimal {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 20;
+
+        /*
+         * 유한소수 판별하기
+         */
+
+        int num = b / gcd(a, b);
+
+        for (int i = 0; i < b; i++) {
+
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+
+            if (num % 5 == 0) {
+                num /= 5;
+            }
+
+        }
+
+        int answer = 2;
+
+        if (num == 1) {
+            answer = 1;
+        }
+
+
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+}
