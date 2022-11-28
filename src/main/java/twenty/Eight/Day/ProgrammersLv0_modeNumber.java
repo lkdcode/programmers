673fd1be5bec20c38a4229941853b4c3d958d6c8
lkdcode <dlrlejr1@naver.com>
2022-11-28 22:27:50 +0900

    ✨Feat : 최빈값 구하기
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Eight/Day/ProgrammersLv0_modeNumber.java
@@ -0,0 +1,50 @@
+package twenty.Eight.Day;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProgrammersLv0_modeNumber {
+    public static void main(String[] args) {
+        int[] array = {0, 0, 1, 1, 2, 2, 3, 3};
+
+        /*
+         * 최빈값 구하기
+         */
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        Collections.sort(list);
+
+        int[] max = new int[(list.get(list.size() - 1)) + 1];
+
+        for (int i = 0; i < list.size(); i++) {
+            max[list.get(i)]++;
+        }
+
+        int number = 0;
+        int index = 0;
+
+        for (int i = 0; i < max.length; i++) {
+            if (max[i] > number) {
+                number = max[i];
+                index = i;
+            }
+        }
+        int answer = index;
+
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < max.length; i++) {
+            arr.add(max[i]);
+        }
+
+        Collections.sort(arr);
+        if (arr.get(arr.size() - 1) == arr.get(arr.size() - 2)) {
+            answer = -1;
+        }
+
+
+    }
+}
