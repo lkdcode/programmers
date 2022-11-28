2279b9c773be84482dac64c90a6e78484f99f8dd
lkdcode <dlrlejr1@naver.com>
2022-11-28 22:27:32 +0900

    ✨Feat : 분수의 덧셈
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Eight/Day/ProgrammersLv0_fractionsAdd.java
@@ -0,0 +1,38 @@
+package twenty.Eight.Day;
+
+public class ProgrammersLv0_fractionsAdd {
+    public static void main(String[] args) {
+        int denum1 = 1;
+        int num1 = 9;
+        int denum2 = 1;
+        int num2 = 6;
+
+        /*
+         * 분수의 덧셈
+         */
+
+        int x = (denum1 * num2) + (denum2 * num1);
+        int y = num1 * num2;
+
+
+        int gcd = gcd(x, y);
+
+        int number1 = x / gcd;
+        int number2 = y / gcd;
+
+        int[] answer = {number1, number2};
+
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+
+}
