4f3814c1090a2462118285d40156af7fc9c28165
lkdcode <dlrlejr1@naver.com>
2022-12-01 23:29:03 +0900

    ✨Feat : 치킨 쿠폰
    프로그래머스 Lv0

---
+++ src/main/java/twenty/Nine/Day/ProgrammersLv0_chickenCoupon.java
@@ -0,0 +1,26 @@
+package twenty.Nine.Day;
+
+public class ProgrammersLv0_chickenCoupon {
+    public static void main(String[] args) {
+        int chicken = 1089;
+
+        /*
+         * 치킨 쿠폰
+         */
+
+        int answer = 0;
+
+
+        while (chicken >= 10) {
+
+            int order = chicken / 10;
+            int rest = chicken % 10;
+
+            chicken = order + rest;
+
+            answer += order;
+        }
+
+
+    }
+}
