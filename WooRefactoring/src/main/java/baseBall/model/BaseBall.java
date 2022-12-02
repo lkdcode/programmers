085fc96e511ce423f689300d6d393f8844b0ace8
lkdcode <dlrlejr1@naver.com>
2022-12-03 03:01:47 +0900

    ✨Feat : 야구 게임을 관리하는 클래스
    <computer> 컴퓨터의 번호를 생성
    <user> 컨트롤러로부터 유저의 번호를 받음
    <playBaseBall> 컴퓨터 번호와 유저의 번호를 비교

---
+++ WooRefactoring/src/main/java/baseBall/model/BaseBall.java
@@ -0,0 +1,18 @@
+package baseBall.model;
+
+public class BaseBall {
+
+    /*
+     *  야구 게임을 관리하는 클래스
+     */
+
+    private void computer() {
+    }
+
+    private void user() {
+    }
+
+    private void playBaseBall(){
+
+    }
+}
