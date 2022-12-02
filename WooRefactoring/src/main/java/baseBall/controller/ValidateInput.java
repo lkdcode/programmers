110f0f7a37d34d1219859982c8fda3b824c62c34
lkdcode <dlrlejr1@naver.com>
2022-12-03 03:04:30 +0900

    ✨Feat : 입력받은 값을 검증하는 클래스

---
+++ WooRefactoring/src/main/java/baseBall/controller/ValidateInput.java
@@ -0,0 +1,25 @@
+package baseBall.controller;
+
+public class ValidateInput {
+
+    /*
+     *  입력받은 값을 검증하는 클래스
+     */
+
+
+    public void validateLength() {
+
+    }
+
+    public void validateNumber() {
+
+    }
+
+    public void validateZero() {
+
+    }
+
+    public void validateDuplication() {
+
+    }
+}
