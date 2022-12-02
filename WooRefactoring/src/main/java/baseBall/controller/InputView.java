7c155b43f895a192f6bb9aad01325da84f312b40
lkdcode <dlrlejr1@naver.com>
2022-12-03 03:02:23 +0900

    ✨Feat : 사용자로부터 입력을 받는 클래스
    <readNumber> 유저로부터 숫자를 입력받는 메소드
    <readCommad> 유저로부터 재시작, 종료를 입력받는 메소드

---
+++ WooRefactoring/src/main/java/baseBall/controller/InputView.java
@@ -0,0 +1,18 @@
+package baseBall.controller;
+
+public class InputView {
+
+    /*
+     *  사용자로부터 입력을 받는 클래스
+     */
+
+    // 사용자로부터 숫자를 입력받는 메소드
+    public void readNumber() {
+
+    }
+
+    // 사용자로부터 재시작,종료 여부를 입력받는 메소드
+    public void readCommand() {
+
+    }
+}
