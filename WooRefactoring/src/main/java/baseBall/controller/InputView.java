9af763d47eefc935830498de4ee8b41f373e910b
lkdcode <dlrlejr1@naver.com>
2022-12-06 02:42:15 +0900

    🔥feat : 사용자로부터 입력을 받는 클래스
    사용자로부터 숫자를 입력받는 메소드
    입력받은 값 검증

--- WooRefactoring/src/main/java/baseBall/controller/InputView.java
+++ WooRefactoring/src/main/java/baseBall/controller/InputView.java
@@ -1,18 +1,28 @@
 package baseBall.controller;
 
-public class InputView {
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
-    /*
-     *  사용자로부터 입력을 받는 클래스
-     */
+/*
+ *  사용자로부터 입력을 받는 클래스
+ */
+public class InputView {
+    private final ValidateInput VALIDATE_INPUT = new ValidateInput();
+    private String inputCommand;
+    private String inputNumber;
 
     // 사용자로부터 숫자를 입력받는 메소드
     public void readNumber() {
+        inputNumber = readLine();
+        VALIDATE_INPUT.validateInputNumber(inputNumber);
+    }
 
+    public String getInputNumber() {
+        return inputNumber;
     }
 
     // 사용자로부터 재시작,종료 여부를 입력받는 메소드
     public void readCommand() {
-
+        inputCommand = readLine();
     }
+
 }
