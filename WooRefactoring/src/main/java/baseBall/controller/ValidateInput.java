cc975222b1ea2fb3abd118e70f0f32c8e1b34cdf
lkdcode <dlrlejr1@naver.com>
2022-12-06 02:44:20 +0900

    🔥feat : 사용자로부터 입력받은 값 검증
    검증 값 추가

--- WooRefactoring/src/main/java/baseBall/controller/ValidateInput.java
+++ WooRefactoring/src/main/java/baseBall/controller/ValidateInput.java
@@ -1,25 +1,51 @@
 package baseBall.controller;
 
+import baseBall.view.MESSAGE;
+
 public class ValidateInput {
+    private String userInput;
 
     /*
      *  입력받은 값을 검증하는 클래스
      */
 
-
-    public void validateLength() {
-
+    public void validateInputNumber(String userInput) {
+        this.userInput = userInput;
+        validateLength();
+        validateNumber();
+        validateZero();
+        validateDuplication();
     }
 
-    public void validateNumber() {
-
+    private void validateLength() {
+        if (userInput.length() != 3) {
+            System.out.println(MESSAGE.ERROR_NUMBER_LENGTH.getView());
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void validateZero() {
-
+    private void validateNumber() {
+        if (!(userInput.matches("^[0-9]*$"))) {
+            System.out.println(MESSAGE.ERROR_NUMBER.getView());
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void validateDuplication() {
-
+    private void validateZero() {
+        if (userInput.contains("0")) {
+            System.out.println(MESSAGE.ERROR_ZERO_NUMBER.getView());
+            throw new IllegalArgumentException();
+        }
     }
+
+    private void validateDuplication() {
+        if (userInput.charAt(0) == userInput.charAt(1) ||
+          userInput.charAt(0) == userInput.charAt(2) ||
+          userInput.charAt(1) == userInput.charAt(2)) {
+            System.out.println(MESSAGE.ERROR_DUPLICATION_NUMBER.getView());
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
