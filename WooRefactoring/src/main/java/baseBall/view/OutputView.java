f66d0823164c95381ae5861976a7ad7675154b51
lkdcode <dlrlejr1@naver.com>
2022-12-06 02:43:23 +0900

    🔥feat : 사용자에게 결과를 보여주는 클래스
    결과를 출력하는 메소드 추가
    낫싱 출력 메소드 추가
    스트라이크 출력 메소드 추가
    볼 출력 메소드 추가

--- WooRefactoring/src/main/java/baseBall/view/OutputView.java
+++ WooRefactoring/src/main/java/baseBall/view/OutputView.java
@@ -1,7 +1,11 @@
 package baseBall.view;
 
-public class OutputView {
+import java.util.List;
 
+public class OutputView {
+    private final int NOTHING = 0;
+    private final int STRIKE = 1;
+    private final int BALL = 2;
     /*
      * 유저에게 게임 진행 상황, 결과를 출력하는 클래스
      */
@@ -13,16 +17,39 @@ public class OutputView {
 
     // 유저에게 입력하라는 메시지
     public void printInput() {
-        System.out.println(MESSAGE.INPUT_NUMBER.getView());
+        System.out.print(MESSAGE.INPUT_NUMBER.getView());
     }
 
     // 유저에게 결과 값을 보여주는 메소드
-    public void printResult() {
+    public void printResult(List<Integer> result) {
+        printNothig(result.get(NOTHING));
+        printStrike(result.get(STRIKE));
+        printBall(result.get(BALL));
+    }
 
+    private void printNothig(int nothingCount) {
+        if (nothingCount != 0) {
+            System.out.println(MESSAGE.NOTHING.getView());
+        }
+    }
+
+    private void printStrike(int strikeCount) {
+        if (strikeCount != 0) {
+            System.out.print(strikeCount + MESSAGE.STRIKE.getView());
+        }
+        if (strikeCount == 3) {
+            printWin();
+        }
+    }
+
+    private void printBall(int ballCount) {
+        if (ballCount != 0) {
+            System.out.println(ballCount + MESSAGE.BALL.getView());
+        }
     }
 
     // 승리, 게임 종료 메시지
-    public void printWin() {
+    private void printWin() {
         System.out.println(MESSAGE.WIN.getView());
     }
 
