51ee8674fa7e30a26e8676b6a97047fccb8d73c1
lkdcode <dlrlejr1@naver.com>
2022-12-06 02:43:45 +0900

    🔥feat : 어플리케이션
    SystemController 클래스 명 변경

--- WooRefactoring/src/main/java/baseBall/Application.java
+++ WooRefactoring/src/main/java/baseBall/Application.java
@@ -1,13 +1,13 @@
 package baseBall;
 
-import baseBall.controller.System;
+import baseBall.controller.SystemController;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        System system = new System();
-        system.System();
+        SystemController baseBall = new SystemController();
+        baseBall.play();
 
     }
 }
