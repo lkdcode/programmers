578670e553057d7127e059bfbafd7f3b8d819068
lkdcode <dlrlejr1@naver.com>
2022-12-06 02:10:57 +0900

    🔥feat : 메시지 ENUM 클래스
    낫싱, 스트라이크, 볼 메시지 추가
    컴퓨터 에러 메시지 추가

--- WooRefactoring/src/main/java/baseBall/view/MESSAGE.java
+++ WooRefactoring/src/main/java/baseBall/view/MESSAGE.java
@@ -6,10 +6,17 @@ public enum MESSAGE {
     WIN("🎉 3개의 숫자를 모두 맞히셨습니다! 게임 종료 🎉"),
     COMMAND("👉 게임을 새로 시작하려면 ☝1, 종료하려면 ✌2 를 입력하세요."),
     QUIT("━━━━━━━━━━━━━━ 게임을 종료하였습니다. ━━━━━━━━━━━━━━"),
+    NOTHING("❌ 낫싱 ❌"),
+    STRIKE(" 스트라이크 👉"),
+    BALL(" 볼 🤜"),
     ERROR_NUMBER("[ERROR] 숫자만 입력해 주세요."),
     ERROR_NUMBER_LENGTH("[ERROR] 3자리의 숫자만 입력 가능합니다."),
     ERROR_DUPLICATION_NUMBER("[ERROR] 중복된 숫자는 입력 불가합니다."),
-    ERROR_ZERO_NUMBER("[ERROR] 숫자 0은 입력하실 수 없습니다.");
+    ERROR_ZERO_NUMBER("[ERROR] 숫자 0은 입력하실 수 없습니다."),
+    ERROR_COMPUTER_LENGTH("[ERROR] 컴퓨터 숫자가 3자리가 아닙니다."),
+    ERROR_COMPUTER_ZERO("[ERROR] 컴퓨터 숫자에 0이 포함됩니다."),
+    ERROR_COMPUTER_DUPLICATION("[ERROR] 컴퓨터 숫자가 중복된 숫자를 가지고 있습니다.");
+
     String view;
 
     MESSAGE(String view) {
