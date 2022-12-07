11ac1bbce1b674c89048a90ec7277675ca704d81
lkdcode <dlrlejr1@naver.com>
2022-12-07 17:06:50 +0900

    🔥feat : 유저로 부터 입력받은 번호를 검증하는 클래스
    상수화 처리

--- WooRefactoring/src/main/java/baseBall/controller/ValidateInput.java
+++ WooRefactoring/src/main/java/baseBall/controller/ValidateInput.java
@@ -3,49 +3,68 @@ package baseBall.controller;
 import baseBall.view.MESSAGE;
 
 public class ValidateInput {
-    private String userInput;
+    private final int COMMAND_LENGTH = 1;
+    private final int NUMBER_LENGTH = 3;
+    private final int FIRST = 0;
+    private final int SECOND = 1;
+    private final int THIRD = 2;
+    private final String ONLY_COMMAND = "^[1-2]*$";
+    private final String ONLY_NUMBERS = "^[0-9]*$";
 
-    /*
-     *  입력받은 값을 검증하는 클래스
-     */
-
-    public void validateInputNumber(String userInput) {
-        this.userInput = userInput;
-        validateLength();
-        validateNumber();
-        validateZero();
-        validateDuplication();
+    public void validateInputNumber(String inputNumber) {
+        validateLength(inputNumber);
+        validateNumber(inputNumber);
+        validateZero(inputNumber);
+        validateDuplication(inputNumber);
     }
 
-    private void validateLength() {
-        if (userInput.length() != 3) {
-            System.out.println(MESSAGE.ERROR_NUMBER_LENGTH.getView());
-            throw new IllegalArgumentException();
-        }
+    public void validateInputCommand(String inputCommand) {
+        validateCommandLength(inputCommand);
+        validateCommandNumber(inputCommand);
     }
 
-    private void validateNumber() {
-        if (!(userInput.matches("^[0-9]*$"))) {
+    private void validateCommandNumber(String inputCommand) {
+        if (!(inputCommand.matches(ONLY_COMMAND))) {
             System.out.println(MESSAGE.ERROR_NUMBER.getView());
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateZero() {
-        if (userInput.contains("0")) {
+    private void validateCommandLength(String inputCommand) {
+        if (inputCommand.length() != COMMAND_LENGTH) {
+            System.out.println(MESSAGE.ERROR_COMMAND.getView());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLength(String inputNumber) {
+        if (inputNumber.length() != NUMBER_LENGTH) {
+            System.out.println(MESSAGE.ERROR_NUMBER_LENGTH.getView());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumber(String inputNumber) {
+        if (!(inputNumber.matches(ONLY_NUMBERS))) {
+            System.out.println(MESSAGE.ERROR_NUMBER.getView());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateZero(String inputNumber) {
+        if (inputNumber.contains("0")) {
             System.out.println(MESSAGE.ERROR_ZERO_NUMBER.getView());
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateDuplication() {
-        if (userInput.charAt(0) == userInput.charAt(1) ||
-          userInput.charAt(0) == userInput.charAt(2) ||
-          userInput.charAt(1) == userInput.charAt(2)) {
+    private void validateDuplication(String inputNumber) {
+        if (inputNumber.charAt(FIRST) == inputNumber.charAt(SECOND) ||
+          inputNumber.charAt(FIRST) == inputNumber.charAt(THIRD) ||
+          inputNumber.charAt(SECOND) == inputNumber.charAt(THIRD)) {
             System.out.println(MESSAGE.ERROR_DUPLICATION_NUMBER.getView());
             throw new IllegalArgumentException();
         }
     }
-
 
 }
