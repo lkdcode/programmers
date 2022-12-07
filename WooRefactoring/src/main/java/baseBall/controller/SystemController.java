f5a65debfd9abeba725ad954ad0b327b7a301da3
lkdcode <dlrlejr1@naver.com>
2022-12-07 17:03:42 +0900

    🔥feat : 게임을 관리하는 컨트롤러
    상수화 처리,
    메소드 분류

--- WooRefactoring/src/main/java/baseBall/controller/SystemController.java
+++ WooRefactoring/src/main/java/baseBall/controller/SystemController.java
@@ -3,7 +3,6 @@ package baseBall.controller;
 import baseBall.model.ComputerNumber;
 import baseBall.model.Referee;
 import baseBall.model.UserNumber;
-import baseBall.view.MESSAGE;
 import baseBall.view.OutputView;
 
 public class SystemController {
@@ -12,29 +11,39 @@ public class SystemController {
     private final UserNumber USER_NUMBER = new UserNumber();
     private final ComputerNumber COMPUTER_NUMBER = new ComputerNumber();
     private final Referee REFEREE = new Referee();
+    private final int WIN_COUNT = 3;
 
-    private boolean win = false;
+    public void joinTheGame() {
+        play();
+        OUTPUT_VIEW.printQuit();
+    }
 
-    public void play() {
-        computer();
-        System.out.println(COMPUTER_NUMBER.getComputerNumbers());
-        enter();
+    private void play() {
+        playComputer();
+        playUser();
         retry();
     }
 
-    private void enter() {
+    private void playComputer() {
+        computer();
+    }
+
+    private void computer() {
+        COMPUTER_NUMBER.makeComputerNumbers();
+        OUTPUT_VIEW.printStart();
+    }
+
+    private void playUser() {
         user();
         referee();
         result();
     }
 
     private void retry() {
-        System.out.println(MESSAGE.COMMAND.getView());
-        if (true) {
+        OUTPUT_VIEW.printCommand();
+        INPUT_VIEW.readCommand();
+        if (INPUT_VIEW.getRetry()) {
             play();
-        }
-        if (false) {
-            //종료
         }
     }
 
@@ -44,24 +53,15 @@ public class SystemController {
         USER_NUMBER.makeUserNumbers(INPUT_VIEW.getInputNumber());
     }
 
-    private void computer() {
-        COMPUTER_NUMBER.makeComputerNumbers();
-        OUTPUT_VIEW.printStart();
-    }
-
     private void referee() {
-        REFEREE.setUserNumbers(USER_NUMBER.getUserNumbers());
-        REFEREE.setComputerNumbers(COMPUTER_NUMBER.getComputerNumbers());
-
-        REFEREE.playBaseBall();
-        OUTPUT_VIEW.printResult(REFEREE.getResult());
+        REFEREE.playBaseBall(USER_NUMBER.getUserNumbers(), COMPUTER_NUMBER.getComputerNumbers());
+        OUTPUT_VIEW.printResult(REFEREE.getStrike(), REFEREE.getBall());
     }
 
     private void result() {
-        if (REFEREE.getStrike() != 3) {
-            enter();
+        if (REFEREE.getStrike() != WIN_COUNT) {
+            playUser();
         }
     }
-
 
 }
